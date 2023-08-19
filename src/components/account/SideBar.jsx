090fcefaf9786d8ae64b7b6ffe156054643dc82b
kerenIy <kerp4real@gmail.com>
2023-08-19 14:22:47 +0100

    Moblie design for side bar

--- src/components/account/SideBar.jsx
+++ src/components/account/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import accountImg from "/src/assets/myemoji.png";
 import SideBarItem from "./SideBarItem";
+import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
@@ -12,15 +13,28 @@ import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 export default function SideBar() {
+  const [toggle, setToggle] = useState("faq-hide");
+
+  const changeToggle = () => {
+    if (toggle == "faq-hide") {
+      setToggle("side-nav-links");
+    } else if (toggle == "side-nav-links") {
+      setToggle("faq-hide");
+    }
+  };
   return (
     <>
       <div className="sidebar">
         <div className="account-details">
-          <img src={accountImg} alt="" className="account-img" />
-          <p>tein</p>
+          <img
+            src={accountImg}
+            alt=""
+            className="account-img"
+            onClick={changeToggle}
+          />
         </div>
 
-        <div className="sidebar-container">
+        <div id="sidebar-container" className={toggle}>
           <div className="side-links">
             <SideBarItem icon={faPaperPlane} title="Budgets" />
             <SideBarItem icon={faPiggyBank} title="Savings" />
