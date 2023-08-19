499e65e1f27751502252099604faf248e8e5d725
kerenIy <kerp4real@gmail.com>
2023-08-19 14:28:02 +0100

    Main component for Account page

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SideBar from "../components/account/SideBar";
+import MainContent from "../components/account/MainContent";
 
 export default function Account() {
   return (
     <>
       <div className="account-page">
         <SideBar />
+        <MainContent />
       </div>
     </>
   );
