4abf264c21c39ed211f2c7996004ba48bb6a530f
kerenIy <kerp4real@gmail.com>
2023-08-22 12:27:23 +0100

    Move home components to home folder

--- src/components/home/Why.jsx
+++ src/components/home/Why.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
-import WhyItem from "../WhyItem";
+import WhyItem from "./WhyItem";
 
 export default function Why() {
   return (
