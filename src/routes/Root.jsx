2b8aee39ed03c15c0c74d2b62bdb55819731765d
kerenIy <kerp4real@gmail.com>
2023-08-22 12:26:54 +0100

    Moved home component to home folder

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/home/Navbar";
 
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import Partners from "../components/Partners";
-import Why from "../components/Why";
-import Footer from "../components/Footer";
-import Banner from "../components/Banner";
-import FAQs from "../components/FAQs";
+import Hero from "../components/home/Hero";
+import Features from "../components/home/Features";
+import Partners from "../components/home/Partners";
+import Why from "../components/home/Why";
+import Footer from "../components/home/Footer";
+import Banner from "../components/home/Banner";
+import FAQs from "../components/home/FAQs";
 
 export default function Root() {
   return (
